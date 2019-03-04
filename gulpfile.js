38b8ddefb11af8a90c64427c4e5673e8883b5701
ayumitk <ayumee528@gmail.com>
2019-03-04 12:16:40 -0800

    update gulp version 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,52 @@
 // Gulp
-const gulp = require("gulp");
+const gulp = require('gulp');
 
 // Local server
-const browserSync = require("browser-sync").create();
+// const browserSync = require("browser-sync").create();
 
 // CSS
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
 const autoprefixer = require("gulp-autoprefixer");
 const csscomb = require("gulp-csscomb");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass');
+
 
 // HTML minify
-const htmlmin = require("gulp-htmlmin");
+// const htmlmin = require("gulp-htmlmin");
 
 // --------------------------------------------------------------------
 // Tasks
 // --------------------------------------------------------------------
+gulp.task('default', function () {
 
-// Local Server + Watch
-gulp.task("server", ["sass", "html"], () => {
-  browserSync.init({
-    server: "./docs/",
-    cors: true
+  return gulp.watch('./src/*.scss', function () {
+
+    return gulp.src('./src/*.scss')
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(autoprefixer({
+        browsers: ['last 2 version'],
+        grid: true
+      }))
+      .pipe(csscomb())
+      .pipe(sass({
+        outputStyle: 'compressed'
+      })
+      .on('error', sass.logError))
+      .pipe(gulp.dest('./docs'));
   });
-
-  gulp.watch("./src/*.scss", ["sass"]);
-  gulp.watch("./src/*.html", ["html"]);
 });
 
-// HTML minify
-gulp.task("html", () => {
-  gulp
-    .src("./src/*.html")
-    .pipe(
-      htmlmin({
-        collapseWhitespace: true
-      })
-    )
-    .pipe(gulp.dest("./docs"))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
-});
 
-// Compile sass into CSS & Auto-inject into browsers
-gulp.task("sass", () =>
-  gulp
-  .src("./src/*.scss")
 
-  .pipe(
-    plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
-    })
-  )
 
-  .pipe(
-    autoprefixer({
-      browsers: ["last 2 versions"],
-      cascade: false
-    })
-  )
 
-  .pipe(csscomb())
 
-  .pipe(
-    sass({
-      outputStyle: "compressed"
-    }).on("error", sass.logError)
-  )
 
-  .pipe(gulp.dest("./docs"))
 
-  .pipe(
-    browserSync.reload({
-      stream: true
-    })
-  )
-);
 
-gulp.task("default", ["server"]);
+
+
+
